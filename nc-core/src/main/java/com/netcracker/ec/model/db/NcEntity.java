bca6a963ab7d4a21456052ae9428d1b23894e320
kahr0817 <kanstantsin.hrynchyk@netcracker.com>
2020-05-05 09:42:29 +0300

    fix close resultSet moved get/set methods to NcObject

--- nc-core/src/main/java/com/netcracker/ec/model/db/NcEntity.java
+++ nc-core/src/main/java/com/netcracker/ec/model/db/NcEntity.java
@@ -1,8 +1,6 @@
 package com.netcracker.ec.model.db;
 
 import com.netcracker.ec.services.db.DbWorker;
-import com.netcracker.ec.services.db.impl.NcParamsServiceImpl;
-import com.netcracker.ec.services.db.impl.NcReferencesServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,27 +20,4 @@ public class NcEntity {
         return id + " - " + name;
     }
 
-    public void setReferenceId(Integer attrId, Integer refId) {
-        new NcReferencesServiceImpl().mergeReference(attrId, id, refId);
-    }
-
-    public void setListValueId(Integer attrId, Integer lvId) {
-        new NcParamsServiceImpl().mergeListValue(id, attrId, lvId);
-    }
-
-    public void setStringValue(Integer attrId, String string) {
-        new NcParamsServiceImpl().mergeValue(id, attrId, string);
-    }
-
-    public Integer getReferenceId(Integer attrId) {
-        return new NcReferencesServiceImpl().selectReference(id, attrId);
-    }
-
-    public Integer getListValueId(Integer attrId) {
-        return new NcParamsServiceImpl().selectListValueId(id, attrId);
-    }
-
-    public String getStringValue(Integer attrId) {
-        return new NcParamsServiceImpl().selectStringValue(id, attrId);
-    }
 }
